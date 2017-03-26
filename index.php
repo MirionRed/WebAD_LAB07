34f3e3ce19f96c5f9aefc881317210d457690633
InnOpyU <innopyu@gmail.com>
2017-03-27 02:02:42 +0800

    LAB07 complete until insert database

--- index.php
+++ index.php
@@ -4,5 +4,18 @@
 </head>
 <body>
   <?php include('connect.php'); ?>
+  <?php include('insert.php'); ?>
+  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>">
+    Subject:
+    <input type="text" name="subject"><br>
+    Message:
+    <input type="text" name="message"><br>
+    Type:
+    <select name="type">
+      <option value="P">Project Updates</opiton>
+      <option value="T">Traffic Announcement<option>
+    </select><br>
+    <input type="submit" value="Submit">
+  </form>
 </body>
 </html>
---
+++ insert.php
@@ -0,0 +1,28 @@
+<?php
+$subject = $message = $type = $posted = "";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $insert = $db->prepare("INSERT INTO announcement (subject, message, type, posted) VALUES (?,?,?,?)");
+  $insert->bind_param("ssss", $subject, $message, $type, $posted);
+
+  date_default_timezone_set("Asia/Kuala_Lumpur");
+  $subject = $_POST["subject"];
+  $message = $_POST["message"];
+  $type = $_POST["type"];
+  $posted = date("Y-m-d H:i:s");
+
+  if($insert->execute()===true){
+    ?> <script>console.log("Record added");</script> <?php
+  } else {
+    ?> <script>console.log("Unable to insert record");</script> <?php
+  }
+  $insert->close();
+} else {
+}
+function input($data){
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+ ?>
